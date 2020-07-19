ee94d4419a50472e8b0a5de7c19fef86f15a47cf
NHD-Zoro <INHDI01.11.18@github.com>
2020-07-19 21:20:38 +0700

    20

--- mysite/settings.py
+++ mysite/settings.py
@@ -112,9 +112,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
